ce37dfe9d90870ad5274b3cd1b11e35ccf5bd57d
nimeshngk <nimeshngk@gmail.com>
2019-03-23 17:50:50 +0530

    login/register functionality

---
+++ src/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import $ from 'jquery';
+
+export class Home extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<h1> This is New Home </h1>
+		);
+	}
+
+}
---
+++ src/login/LoginBox.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './login.css';
+import $ from 'jquery';
+import { Redirect } from 'react-router';
+
+
+let appurl = "http://localhost:1433"
+
+export class LoginBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "", errors: [] , redirect: false};
+  }
+  showValidationErr(elm, msg) {
+    this.setState(prevState => ({
+      errors: [...prevState.errors, { elm, msg }]
+    }));
+  }
+  clearValidationErr(elm) {
+    this.setState(prevState => {
+      let newArr = [];
+      for (let err of prevState.errors) {
+        if (elm != err.elm) {
+          newArr.push(err);
+        }
+      }
+      return { errors: newArr };
+    });
+  }
+  onUsernameChange(param) {
+    this.setState({ username: param.target.value });
+    this.clearValidationErr("username");
+  }
+  onPasswordChange(param) {
+    this.setState({ password: param.target.value });
+    this.clearValidationErr("password");
+  }
+  submitLogin(param) {
+    
+    if (this.state.username == "") {
+      this.showValidationErr("username", "username cannot be empty");
+    }
+    if (this.state.password == "") {
+      this.showValidationErr("password", "password cannot be empty");
+    }
+    console.log("login going"); 
+    if(this.state.username !== "" && this.state.password !== ""){
+      // jquery
+      $.ajax({
+        url: appurl + '/account/logIn',
+        method: 'POST',
+        data:{
+          email: this.state.username,
+          password: this.state.password
+        },
+        success: function(result){
+          if(result.login){
+            console.log("login success");
+            this.setState({ redirect: true });
+          }
+          else{
+            console.log("login failed");
+          }
+        }.bind(this)
+      });
+
+    }
+  }
+
+  render() {
+    const { redirect } = this.state;
+    let usernameErr = null,
+      passwordErr = null;
+    for (let err of this.state.errors) {
+      if (err.elm == "username") {
+        usernameErr = err.msg;
+      }
+      if (err.elm == "password") {
+        passwordErr = err.msg;
+      }
+    }
+    if(redirect){
+      console.log("redirecting");
+      return <Redirect to='/Home/'/>
+    }
+    return (
+      <div className="inner-container">
+        <div className="header">Login</div>
+        <div className="box">
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="Enter Username"
+              onChange={this.onUsernameChange.bind(this)}
+            />
+            <small className="danger-error">
+              {usernameErr ? usernameErr : ""}
+            </small>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Enter Password"
+              onChange={this.onPasswordChange.bind(this)}
+            />
+            <small className="danger-error">
+              {passwordErr ? passwordErr : ""}
+            </small>
+          </div>
+
+          <button
+            type="button"
+            className="login-btn"
+            onClick={this.submitLogin.bind(this)}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
